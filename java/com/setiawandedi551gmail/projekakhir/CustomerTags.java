0f3100cc054a09081d714a0fe196e491aed94611
dedi setiawan <39402047+dedisetiawan115@users.noreply.github.com>
2018-12-27 23:27:54 +0700

    Add files via upload

---
+++ java/com/setiawandedi551gmail/projekakhir/CustomerTags.java
@@ -0,0 +1,19 @@
+package com.setiawandedi551gmail.projekakhir;
+
+public class CustomerTags {
+
+    public static final String TAG_FIRSTNAME = "firstName";
+    public static final String TAG_LASTNAME = "lastName";
+    public static final String TAG_AGE = "age";
+    public static final String TAG_ADDRESS = "address";
+
+    public static final String TAG_STREET_ADDRESS = "streetAddress";
+    public static final String TAG_CITY = "city";
+    public static final String TAG_STATE = "state";
+    public static final String TAG_POSTAL_CODE = "postalCode";
+
+    public static final String TAG_PHONE = "phoneNumber";
+    public static final String TAG_TYPE = "type";
+    public static final String TAG_NUMBER = "number";
+
+}
